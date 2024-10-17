e15b1d6b907e4c255048d2f4192977662e52f1c7
Angelldca <angeluisdelcollado@gmail.com>
2024-10-17 10:12:36 -0400

    Sum of two integers

---
+++ src/main/java/org/example/SumofTwoIntegers.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.*;
+
+public class SumofTwoIntegers {
+    public static void main(String []args){
+        int a = 2;
+        int b = 3;
+        System.out.println(new SumofTwoIntegers().getSum(a,b));
+    }
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+
+        }
+
+        return a;
+    }
+}
